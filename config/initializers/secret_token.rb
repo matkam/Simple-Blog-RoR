e4468d24bb7b995a6375902e70b1dd717ccbe3ba
Matt K <matkam@gmail.com>
2012-05-19 20:44:50 -0700

    Hello world

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MatkamNet::Application.config.secret_token = 'b6463806f2c092c27078d41bdb47fe2ee37279d587bf6b43a7e637c1c1b6784b8ae1ac617f0573000556dd10adcbfa7dbea59faaaaf37019c8337450d3bc6dc9'
