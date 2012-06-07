8a6c02460b4d6ee2f554f65986fa1a48d71b7123
Matt K <matkam@gmail.com>
2012-06-06 19:34:52 -0700

    Google Analytics try 2

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 MatkamNet::Application.initialize!
-
-# Google Analytics
-config.gem 'rack-google-analytics', :lib => 'rack/google-analytics', :domain => 'matkam.net', :multiple => true
-config.middleware.use Rack::GoogleAnalytics, :tracker => 'UA-32447269-1'
